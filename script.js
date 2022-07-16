df847702ac08f8be510347903b0b4e96145e7e26
Liamo <liamwaters15@gmail.com>
2022-07-16 18:41:13 +0100

    Tweaks lol

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 const targetCharacter = document.getElementById("Target");
 const buttonHolder = document.getElementById("buttons");
+const meanings = document.getElementById("Meanings");
 
 let kanjiJSON;
 let kanjiKeys;
 
 let isLoaded = false;
+let kanjiPacks = [];
 
 LoadKanji();
 
 function Main() {
   isLoaded = true;
 
-  RandomKanji();
-
-  GenerateRandomButtons();
+  PackKanji();
+  GenerateKanji(5);
+  GenerateKun(5);
 }
 
-function GenerateRandomButtons() {
+function PackKanji() {
+  for (let i = 0; i < 5; i++) {
+    kanjiPacks[i] = [];
+    for (let j = 0; j < kanjiKeys.length; j++) {
+      if (kanjiJSON[kanjiKeys[j]]["jlpt_new"] == i + 1) {
+        let temp = {
+          character: kanjiKeys[j],
+          data: kanjiJSON[kanjiKeys[j]],
+        };
+        kanjiPacks[i].push(temp);
+      }
+    }
+  }
+}
+
+function GenerateKun(level) {
   let noOfButtons = 16;
 
   let buttons = [];
 
   buttonHolder.innerHTML = "";
+
   for (let i = 0; i < noOfButtons; i++) {
-    let randomIndex = Math.floor(Math.random() * kanjiKeys.length);
-    let randomChar = kanjiKeys[randomIndex];
-    let str = kanjiJSON[randomChar]["readings_kun"];
-    if (str == ""){ i--; continue;}
+    let pack = kanjiPacks[level - 1];
+    let randomIndex = Math.floor(Math.random() * pack.length);
+    let str = pack[randomIndex]["data"]["readings_kun"];
+    if (str == "") {
+      i--;
+      continue;
+    }
 
     buttons.push(document.createElement("button"));
     buttons[i].innerHTML = str;
@@ -34,10 +55,13 @@ function GenerateRandomButtons() {
   }
 }
 
-function RandomKanji() {
-  let randomIndex = Math.floor(Math.random() * kanjiKeys.length);
-  let character = kanjiKeys[randomIndex];
+function GenerateKanji(level) {
+  let pack = kanjiPacks[level - 1];
+  let randomIndex = Math.floor(Math.random() * pack.length);
+  let character = pack[randomIndex]["character"];
   targetCharacter.innerHTML = character;
+  meanings.innerHTML = kanjiJSON[character]["wk_meanings"];
+  
   return character;
 }
 
